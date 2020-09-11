c03b84fe6cb4183359c90eaa70074d03439f01d9
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-09-11 10:27:03 -0300

    Parte 2 concluída

--- bloco_5/dia_4/script.js
+++ bloco_5/dia_4/script.js
@@ -1,14 +1,44 @@
-window.onload = () => {
+window.onload = function () {
+  bgColor = localStorage.bgColor;
+  textColor = localStorage.textColor;
+  textSize = localStorage.textSize;
+  fontFamily = localStorage.fontFamily;
+  lineHeight = localStorage.lineHeight;
 
-    bgColor = localStorage.getItem("bgColor");
-    textColor = localStorage.getItem("textColor");
-    textSize = localStorage.getItem("textSize");
-    fontFamily = localStorage.getItem("fontFamily");
-    lineHeight = localStorage.getItem("lineHeight");
+  if (
+    bgColor != null &&
+    textColor != null &&
+    textSize != null &&
+    fontFamily != null &&
+    lineHeight != null
+  ) {
+    applyStyle();
+  }
 
-function addCss() {
-    bgColor = localStorage.setItem("bgColor", "white");
-    bgColor = localStorage.setItem("texColor", "blue");
-    bgColor = localStorage.setItem("textSize", "20px");
-}
-}
+  function applyStyle() {
+    let body = document.querySelector("body");
+    let p = document.getElementsByTagName("p");
+    body.style.backgroundColor = bgColor;
+    for (let index = 0; index < p.length; index += 1){
+      p[index].style.color = textColor;
+      p[index].style.textSize = textSize;
+      p[index].style.fontFamily = fontFamily;
+      p[index].style.lineHeight = lineHeight;
+    }
+
+  }
+
+  function addCss() {
+    localStorage.bgColor = "white";
+    localStorage.textColor = "blue";
+    localStorage.textSize = "20px";
+    localStorage.fontFamily = "monospace";
+    localStorage.lineHeight = "40px";
+  }
+
+  const btnAddStyle = document.querySelector("#btnAddStyle");
+  btnAddStyle.addEventListener("click", addCss);
+
+  const btnRemoveStyle = document.querySelector("#btnRemoveStyle");
+  btnRemoveStyle.addEventListener("click", () => localStorage.clear());
+};
