cd78f4e167c96d810ca34b333a7d0dd8575f3d87
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-10-08 13:36:34 -0300

    Exercício 5  concluído.

--- bloco_8/dia_3/05.js
+++ bloco_8/dia_3/05.js
@@ -1,16 +1,25 @@
-    
 const assert = require('assert');
+const { access } = require('fs/promises');
 
 const names = [
-  'Aanemarie',  'Adervandes',   'Akifusa',
-  'Abegildo',   'Adicellia',    'Aladonata',
-  'Abeladerco', 'Adieidy',  'Alarucha',
+  'Aanemarie',
+  'Adervandes',
+  'Akifusa',
+  'Abegildo',
+  'Adicellia',
+  'Aladonata',
+  'Abeladerco',
+  'Adieidy',
+  'Alarucha',
 ];
 
-
-
 function containsA() {
-
+  return names.reduce((acc, curr) => {
+    const numberOfLettersAByWord = curr.toUpperCase().split('').reduce((accumulator, current) => {
+      return current === 'A' ? accumulator + 1 : accumulator;
+      }, 0);
+     return acc + numberOfLettersAByWord;
+  }, 0);
 }
 
 assert.deepEqual(containsA(), 20);
