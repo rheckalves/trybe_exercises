f788574a9d866b22fccfebabba476304de33f182
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-11-05 16:15:43 -0300

    criado HelloWorld React.

---
+++ bloco_11/dia_1/hello-world-react/src/HelloWorld.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+    render() {
+        return (
+            <h1 className="Hello-World">Hello World!</h1>
+        );
+    }
+}
+
+export default HelloWorld;
